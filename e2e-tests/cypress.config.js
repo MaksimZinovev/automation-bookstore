d4b97fad2fef88a45ed527b2bcb33b018b21d653
Maksim Zinovev <maksimzinovevsubmit@gmail.com>
2023-01-22 10:29:40 +0600

    reinstall cypress to fix support commands error

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    e2e: {
+  e2e: {
     pageLoadTimeout: 120000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
